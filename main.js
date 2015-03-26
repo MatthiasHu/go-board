8426ee9e28c99307a69dc10f810f930490f895bf
Matthias Hutzler <mathias.ralph.hutzler@student.uni-augsburg.de>
2015-03-26 17:43:34 +0100

    internal game history and undo button

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
-"strict mode";
+"use strict";
 
 var size = 9;
-var board = [];
 var BLACK="BLACK", WHITE="WHITE";
-var activePlayer = BLACK;
-var scores = {WHITE:0, BLACK:0}; 
+
+var ui = {fields: []};
+
+var game = {};
+game.board = [];
+game.activePlayer = BLACK;
+game.scores = {WHITE:0, BLACK:0}; 
+
+var gameHistory = [];
 
 
 function onLoad() {
@@ -13,36 +19,52 @@ function onLoad() {
 		HTMLBoard();
 	// initialize board variable
 	for (var x=0; x<size; x++) {
-		board[x] = [];
+		game.board[x] = [];
 		for (var y=0; y<size; y++) {
-			board[x][y] = {};
-			board[x][y].content = null;
-			board[x][y].ui = document.getElementById(fieldId(x, y));
+			game.board[x][y] = null;
 		}
 	}
-	// display initial scores (0 both)
-	// and initial active player (black)
-	refreshScores();
-	refreshActivePlayerIndicators();
+	// get DOM elements
+	for (var x=0; x<size; x++) {
+		ui.fields[x] = [];
+		for (var y=0; y<size; y++) {
+			ui.fields[x][y] =
+				document.getElementById(fieldId(x, y));
+		}
+	}
+	// refresh all ui
+	refreshUI();
 }
 
+function refreshUI() {
+	refreshScores();
+	refreshActivePlayerIndicators();
+	refreshAllFields();
+}
+function refreshAllFields() {
+	for (var x=0; x<size; x++) {
+		for (var y=0; y<size; y++) {
+			refreshField(x, y);
+		}
+	}
+}
+
+
 function onFieldClick(x, y) {
-	switch (board[x][y].content) {
+	switch (game.board[x][y]) {
 		case null: // place a stone
-			setField(x, y, activePlayer);
+			pushHistory();
+			setField(x, y, game.activePlayer);
 			flipActivePlayer();
 			break;
-		case activePlayer: // remove captured stones
-			captureGroup(x, y, activePlayer);
-			break;
-		case flip(activePlayer): // for undoing turns
-			setField(x, y, null);
-			flipActivePlayer();
+		case game.activePlayer: // remove captured stones
+			pushHistory();
+			captureGroup(x, y, game.activePlayer);
 			break;
 	}
 }
 function flipActivePlayer() {
-	activePlayer = flip(activePlayer);
+	game.activePlayer = flip(game.activePlayer);
 	refreshActivePlayerIndicators();
 }
 function flip(playerColor) {
@@ -54,10 +76,37 @@ function flip(playerColor) {
 	}
 }
 
+function pushHistory() {
+	pushList(gameHistory, copyGame(game));
+}
+function onUndo() {
+	if (!gameHistory[0]) return;
+	game = popList(gameHistory);
+	refreshUI();
+}
+function copyGame(g) {
+	var gg = {};
+	gg.activePlayer = g.activePlayer;
+	gg.scores = {WHITE: g.scores[WHITE]
+		    ,BLACK: g.scores[BLACK]};
+	gg.board = [];
+	// copy board variable
+	for (var x=0; x<size; x++) {
+		gg.board[x] = [];
+		for (var y=0; y<size; y++) {
+			gg.board[x][y] = g.board[x][y];
+		}
+	}
+	return gg;
+}
+
 function setField(x, y, value) {
 	if (!(value==null || value==BLACK || value==WHITE)) return;
-	board[x][y].content = value;
-	board[x][y].ui.style = fieldContentToStyle(value);
+	game.board[x][y] = value;
+	refreshField(x, y);
+}
+function refreshField(x, y) {
+	ui.fields[x][y].style = fieldContentToStyle(game.board[x][y]);
 }
 function fieldContentToStyle(value) {
 	switch (value) {
@@ -75,11 +124,11 @@ function fieldContentToStyle(value) {
 
 function captureGroup(x, y, color) {
 	if (x<0 || x>=size || y<0 || y>=size) return;
-	if (board[x][y].content != color) return;
+	if (game.board[x][y] != color) return;
 	setField(x, y, null);
-	scores[flip(color)] += 1;
+	game.scores[flip(color)] += 1;
 	refreshScores();
-	for (neighbour of [{x:x, y:y-1}
+	for (var neighbour of [{x:x, y:y-1}
 		   ,{x:x, y:y+1}
 		   ,{x:x-1, y:y}
 		   ,{x:x+1, y:y}]) {
@@ -108,15 +157,15 @@ function HTMLBoard() {
 
 function refreshScores() {
 	document.getElementById("score_black_div").innerHTML =
-		scores[BLACK];
+		game.scores[BLACK];
 	document.getElementById("score_white_div").innerHTML =
-		scores[WHITE];
+		game.scores[WHITE];
 }
 function refreshActivePlayerIndicators() {
 	var div = document.getElementById("activePlayerIndicator_black_div");
-	div.style.display = activePlayer==BLACK ? "block" : "none";
+	div.style.display = game.activePlayer==BLACK ? "block" : "none";
 	div = document.getElementById("activePlayerIndicator_white_div");
-	div.style.display = activePlayer==WHITE ? "block" : "none";
+	div.style.display = game.activePlayer==WHITE ? "block" : "none";
 }
 
 function fieldId(x, y) {
@@ -125,9 +174,25 @@ function fieldId(x, y) {
 
 function fieldStyle(x, y) {
 	str = "";
-	if (board[x][y].content==BLACK)
+	if (game.board[x][y].content==BLACK)
 		str += "background: #101010;";
 	else if (board[x][y].content==WHITE)
 		str += "background: #f0f0f0;";
 	return str;
+}
+
+// list/array utility:
+function pushList(list, head) {
+	for (var i=list.length-1; i>=0; i--) {
+		list[i+1] = list[i];
+	}
+	list[0] = head;
+}
+function popList(list) {
+	var head = list[0];
+	for (var i=0; i<list.length-1; i++) {
+		list[i] = list[i+1];
+	}
+	delete list[list.length-1];
+	return head;
 }
